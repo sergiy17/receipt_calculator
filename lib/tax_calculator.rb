b7f2480983445496263ef596fdf4f242e1bec605
Sergiy Parkhuts <serparkhuts17@gmail.com>
2025-02-26 18:19:18 +0200

    Create 'TaxCalculator' lib

---
+++ lib/tax_calculator.rb
@@ -0,0 +1,27 @@
+require 'bigdecimal'
+
+class TaxCalculator
+  EXEMPT_PRODUCTS = %w[book chocolate pills].freeze
+  IMPORTED_PRODUCTS = %w[imported].freeze
+  ROUNDING_FACTOR = 20
+
+  def self.calculate(product_name, price, quantity)
+    intermediate_tax = tax_rate(product_name) * price / 100
+    rounded_intermediate_tax = round_to_nearest_5_cents(intermediate_tax)
+    final_tax = rounded_intermediate_tax * quantity
+    final_tax.round(2, BigDecimal::ROUND_HALF_UP)
+  end
+
+  private
+
+  def self.tax_rate(product_name)
+    base_tax = 10
+    base_tax = 0 if EXEMPT_PRODUCTS.any? { |word| product_name.include?(word) }
+    base_tax += 5 if IMPORTED_PRODUCTS.any? { |word| product_name.include?(word) }
+    BigDecimal(base_tax.to_s)
+  end
+
+  def self.round_to_nearest_5_cents(value)
+    BigDecimal(((value * ROUNDING_FACTOR).ceil.to_f / ROUNDING_FACTOR).to_s)
+  end
+end
