0e3c25ac78da3dc56a782d005d25f751ca3886c4
Sergiy Parkhuts <serparkhuts17@gmail.com>
2025-02-26 17:29:05 +0200

    Create spec test for 'ShoppingBucketParser'

--- lib/shopping_bucket_parser.rb
+++ lib/shopping_bucket_parser.rb
@@ -1,6 +1,12 @@
 require 'bigdecimal'
 
 class ShoppingBucketParser
+  # @param input [String] Shopping bucket as a string
+  # @return [Array<Hash>] An array of item hashes, where each hash contains:
+  #   - :quantity [Integer] Items quantity
+  #   - :product_name [String] Product's name
+  #   - :price [BigDecimal] Product's price
+
   def self.parse(input)
     input.split("\n").map do |line|
       quantity, *product_name_parts, price_str = line.split(' ')
